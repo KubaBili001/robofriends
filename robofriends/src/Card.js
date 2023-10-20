b12e55c223930ddd414de63a1defcfb7c35bec7a
KubaBili001 <kubabili.wawa@gmail.com>
2023-10-20 23:42:09 +0200

    Finished Card functionality

--- robofriends/src/Card.js
+++ robofriends/src/Card.js
@@ -1,10 +1,12 @@
+import './Card.css';
+
 function Card({name, email, id}){
     return(
-        <div className="tc bg-light-green dib br3 pa3 ms2 grow bw2 shadow-5">
-            <img src='https://robohash.org/${id}?200x200' alt='robot image' />
+        <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+            <img src={`https://robohash.org/${id}?200x200`} alt='robot image' />
             <div>
-                <h2>name</h2>
-                <p>email</p>
+                <h2>{name}</h2>
+                <p>{email}</p>
             </div>
         </div>
     );
